eb405507dff0af9f837d0467f29884a0ab7cfdd9
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-08 20:16:54 +0200

    Add forloop in home.html, remove redundant code in the same template.

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,11 @@
+#_________________________________________________________________________  SHOP/VIEWS.PY  -->
 from django.shortcuts import render
+from products.models import Product
 
 def home(request):
-      return render(request, 'home.html')
+      products = Product.objects.all().filter(is_available=True)
+
+      context = {
+            'products': products, 
+      }
+      return render(request, 'home.html', context)
