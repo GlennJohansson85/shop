27c9026cb2677a7ead2668cff3c7a06e4809cf7f
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-08 20:47:59 +0200

    Remove temporary folder due to its not permananent
    - Create products.html - Showing all products. Not done

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+#_________________________________________________________________________  PRODUCTS/URLS.PY  -->
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.products, name='products'),
+]
+
+
+
--- shop/urls.py
+++ shop/urls.py
@@ -1,20 +1,6 @@
-"""shop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+#_________________________________________________________________________  SHOP/URLS.PY  -->
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from . import views
 from django.conf.urls.static import static
@@ -23,6 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
