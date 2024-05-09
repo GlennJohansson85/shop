2e977c717c004a4043ff94c6fee0d00ab87aec1a
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-09 13:31:09 +0200

    Displays products by category using slug

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.products, name='products'),
+    path('<slug:category_slug>/', views.products, name='products_by_category'),
 ]
 
 
