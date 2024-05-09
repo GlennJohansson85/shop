dfea2222f6038550c651e1f0a3e9699d4e1af619
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-09 16:08:56 +0200

    Clicking on productcard and product links now takes user to the specific product detail

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 #_________________________________________________________________________  PRODUCTS/MODELS.PY  -->
 from django.db import models
 from category.models import Category
+from django.urls import reverse
 
 
 class Product(models.Model):
@@ -14,6 +15,9 @@ class Product(models.Model):
       category          = models.ForeignKey(Category, on_delete=models.CASCADE) # Deletes incl. products
       created_date      = models.DateTimeField(auto_now_add=True)
       modified_date     = models.DateTimeField(auto_now=True)
+
+      def get_url(self):
+            return reverse('product_detail', args=[self.category.slug, self.slug])
 
       def __str__(self):
             return self.product_name
