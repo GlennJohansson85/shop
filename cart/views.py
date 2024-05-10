1f12312931607c8af9e09aadabecfb15969ab831
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-10 22:36:23 +0200

    Create view for getting cart items, total and quatity, implement the data to cart.html. Not sure why "each" product prize is not showing

--- cart/views.py
+++ cart/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from products.models import Product
 from .models import Cart, CartItem
 
-from django.http import HttpResponse
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -14,10 +13,8 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id) # GET product id
-
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # GET cart by cart_id in session
-
     except Cart.DoesNotExist:
         cart = Cart.objects.create(
             cart_id = _cart_id(request)
@@ -35,11 +32,22 @@ def add_cart(request, product_id):
             cart = cart,
         )
         cart_item.save()
-    return HttpResponse(cart_item.product)
-    exit()
     return redirect('cart')
 
 
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectNotExist:
+        pass # Do nothing
 
-def cart(request):
-    return render(request, 'products/cart.html')
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+    }
+    return render(request, 'products/cart.html', context)
