8ccbc31d86691782982f0c39e38471b2fd5a6fd7
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-12 00:03:14 +0200

    RegistrationForm
    - Remove fields from template
    - Use Django placeholders instead
    - Customise Registration form in the template

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,25 @@
+#_________________________________________________________________________  ACCOUNTS/FORMS.PY
 from django import forms
 from .models import Account
 
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Choose a strong password'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': '<--- Same as this'
+    }))
+
     class Meta:
-        model = Account
-        fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
+        model   = Account
+        fields  = ['first_name', 'last_name', 'email', 'password', 'phone_number']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'David'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Hasselhoff'
+        self.fields['email'].widget.attrs['placeholder'] = 'David.Hasselhoff@hotmail.com'
+        self.fields['phone_number'].widget.attrs['placeholder'] = '+1 555-123-4567'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
