40f3e740b1a99eb485628ba54860aa568ffc9467
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-15 01:11:59 +0200

    Order function
    - Order success message sent to purchasers mail
    - Purchase details shown on site when transactions has been performed.
    - Paypal transactions works as a charm

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
 #_________________________________________________________________________  ORDERS/VIEWS.PY
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from cart.models import CartItem
 from .forms import OrderForm
 import datetime
@@ -7,12 +8,16 @@ from .models import Order, Payment, OrderProduct
 
 import json
 
+from products.models import Product
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
 #___________________________________________________________  CLASS PAYMENTS
 def payments(request):
     body = json.loads(request.body)
     order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
-    print(body)
-    # Save transaction details
+
+    # Store transaction details inside Payment model
     payment = Payment(
         user = request.user,
         payment_id = body['transID'],
@@ -26,8 +31,9 @@ def payments(request):
     order.is_ordered = True
     order.save()
 
-    # Move cart items to order product table
+    # Move the cart items to Order Product table
     cart_items = CartItem.objects.filter(user=request.user)
+
     for item in cart_items:
         orderproduct = OrderProduct()
         orderproduct.order_id = order.id
@@ -39,17 +45,37 @@ def payments(request):
         orderproduct.ordered = True
         orderproduct.save()
 
+        cart_item = CartItem.objects.get(id=item.id)
+        product_variation = cart_item.variations.all()
+        orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+        orderproduct.variations.set(product_variation)
+        orderproduct.save()
 
 
-    # Reduce quantity
+        # Reduce the quantity of the sold products
+        product = Product.objects.get(id=item.product_id)
+        product.stock -= item.quantity
+        product.save()
 
-    # clear cart
+    # Clear cart
+    CartItem.objects.filter(user=request.user).delete()
 
-    # Send email to purchaser - order successful
+    # Send order recieved email to customer
+    mail_subject = 'Thank you for your order!'
+    message = render_to_string('orders/order_recieved_email.html', {
+        'user': request.user,
+        'order': order,
+    })
+    to_email = request.user.email
+    send_email = EmailMessage(mail_subject, message, to=[to_email])
+    send_email.send()
 
-    # Send order details  - JSON Response
-
-    return render(request, 'orders/payments.html')
+    # Send order number and transaction id back to sendData method via JsonResponse
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
+    return JsonResponse(data)
 
 
 #___________________________________________________________  CLASS PLACE_ORDER
