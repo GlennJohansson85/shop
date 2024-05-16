71479972c39bdfbe2ff5a8e1e895ff4372bdbd68
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-16 04:54:09 +0200

    Pushed to SQL

--- shop/settings.py
+++ shop/settings.py
@@ -1,16 +1,13 @@
-#_________________________________________________________________________  SHOP/SETTINGS.PY  -->
+#  SHOP/SETTINGS.PY
 import os, env
+import dj_database_url
 from pathlib import Path
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -18,8 +15,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -66,23 +61,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'shop.wsgi.application'
-
 AUTH_USER_MODEL = 'accounts.Account'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
+   'default': {
         'ENGINE':'django.db.backends.sqlite3',
         'NAME':BASE_DIR / 'db.sqlite3',
     }
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,18 +89,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE     = 'UTC'
 USE_I18N      = True
 USE_L10N      = True
 USE_TZ        = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL          = '/static/'
 STATIC_ROOT         = BASE_DIR/'static'
@@ -121,10 +104,6 @@ STATICFILES_DIRS    = [
 
 MEDIA_URL           = '/media/'
 MEDIA_ROOT          = os.path.join(BASE_DIR, 'media')
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 
 from django.contrib.messages import constants as messages
@@ -138,5 +117,6 @@ EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'glenncoding@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD','')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
