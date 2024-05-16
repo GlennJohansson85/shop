1ba1653388cae3582449dd5e4cde5659b4744dcf
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-16 03:15:34 +0200

    Dashboard - order history
    * User can now click on their orders in the dashboard "my orders" and see the details of that orders. Add dynamic url to fetch the order number, trans id, status and subtotal.
    
    * Note: When ordering i noticed paypal is rather slow. Not sure why but either way the transaction will go through (Could be because I changed my test email to my regular email adress.

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistrationForm, UserForm, UserProfileForm
 from .models import Account, UserProfile
-from orders.models import Order
+from orders.models import Order, OrderProduct
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -218,7 +218,7 @@ def edit_profile(request):
     return render(request, 'accounts/edit_profile.html', context)
 
 
-#___________________________________________________________  DEF CHANGE_PASSOWRD
+#___________________________________________________________  DEF CHANGE_PASSOWORD
 @login_required(login_url='signin')
 def change_password(request):
     if request.method == 'POST':
@@ -242,3 +242,20 @@ def change_password(request):
             return redirect('change_password')
 
     return render(request, 'accounts/change_password.html')
+
+
+#___________________________________________________________  DEF ORDER_DETAIL
+@login_required(login_url='signin')
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+
+    context = {
+        'order_detail': order_detail,
+        'order': order,
+        'subtotal': subtotal,
+    }
+    return render(request, 'accounts/order_detail.html', context)
