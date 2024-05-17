f4c3da6f1c07ec378cb62b147d92deb53abcd20f
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-17 08:21:32 +0200

    Deploy to Heroku

--- shop/settings.py
+++ shop/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['p5-ishop', 'localhost']
 
 
 INSTALLED_APPS = [
